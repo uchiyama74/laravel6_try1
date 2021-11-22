80cc26702bd19714388753f3ad23a10eb5b1a09d
uchiyama74 <uchiyama-@mub.biglobe.ne.jp>
2021-11-23 07:51:25 +0900

    Gateを試した。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use App\Post;
 use App\Http\Requests\StorePost;
 
 class PostController extends Controller
@@ -14,6 +16,23 @@ class PostController extends Controller
 
     public function store(StorePost $request)
     {
+        Gate::before(function ($user, $ability) {
+            // return true;
+        });
+
+        Gate::after(function ($user, $ability, $result, $arguments) {
+            // logger('---Gate::after result: ' . var_export($result, true));
+        });
+
+        // if (Gate::denies('my-gate1')) {
+        //     return view('post.create', ['resultMsg' => 'あなたは登録できません。']);
+        // }
+        Gate::authorize('my-gate1');
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
 
         return view('post.create', [
             'resultMsg' => '登録しました。'
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Http\Request;
 use App\User;
 
@@ -31,6 +32,16 @@ class AuthServiceProvider extends ServiceProvider
         Auth::viaRequest('my-auth', function ($request) {
 // logger('---my-auth');
             return User::where('id', $request->query('userId'))->first();
+        });
+
+        Gate::define('my-gate1', function ($user) {
+// logger('---my-gate1');
+            // return $user->id > 1;
+            if ($user->id !== 3) {
+                return Response::allow();
+            } else {
+                return Response::deny("{$user->name}には権限がありません。");
+            }
         });
     }
 }
