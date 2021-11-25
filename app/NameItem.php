b9a423d2d0f4839e7d869e99c6acbebdac3edfef
uchiyama74 <uchiyama-@mub.biglobe.ne.jp>
2021-11-25 11:11:17 +0900

    モデルのスコープ機能を試した。

--- app/NameItem.php
+++ app/NameItem.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class NameItem extends Model
 {
-    //
+    public function scopeHasOwner($query)
+    {
+        return $query->whereNotNull('owner_id');
+    }
 }
